749c37d60e87d86710a25f0b16933fc6e60df400
Amanda Iaria <amanda@wearehmc.com>
2016-01-07 14:51:45 -0500

    Fixing script issues

--- assets/script/build/build.js
+++ assets/script/build/build.js
@@ -1,35 +1,37 @@
-var $ = jQuery;
-
-$(document).ready(function(){
-	$('.multiple-items').slick({
-  		infinite: true,
-  		slidesToShow: 3,
-  		slidesToScroll: 3
-	});
 
 
-	$('.instacenter').slick({
-  		centerMode: true,
-  		centerPadding: '60px',
-  		slidesToShow: 3,
-  		responsive: [
-    	{
-      		breakpoint: 768,
-      		settings: {
-        		arrows: false,
-        		centerMode: true,
-        		centerPadding: '40px',
-        		slidesToShow: 3
-      	}	
-    	},
-    	{
-      		breakpoint: 480,
-      		settings: {
-        		arrows: false,
-        		centerMode: true,
-        		centerPadding: '40px',
-        		slidesToShow: 1
-      		}
-    	}]
-	});
-});
+(function($){
+
+          $('.multiple-items').slick({
+              infinite: true,
+              slidesToShow: 3,
+              slidesToScroll: 3
+          });
+
+
+          $('.instacenter').slick({
+              centerMode: true,
+              centerPadding: '60px',
+              slidesToShow: 5,
+              initialSlide: 12,
+              
+              responsive: [{
+                  breakpoint: 768,
+                  settings: {
+                    arrows: false,
+                    centerMode: true,
+                    centerPadding: '40px',
+                    slidesToShow: 3
+                  } 
+              },
+              {
+                  breakpoint: 480,
+                  settings: {
+                    arrows: false,
+                    centerMode: true,
+                    centerPadding: '40px',
+                    slidesToShow: 1
+                  }
+              }]
+          });
+}(jQuery))
